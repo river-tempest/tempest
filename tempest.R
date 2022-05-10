0c08c0fa7649244a614c177afb9354dd4d1f0d21
Daniel Philippus <dphilippus@mines.edu>
2022-05-10 10:47:43 -0600

    Updates to case study and tuning

--- tempest.R
+++ tempest.R
@@ -17,10 +17,11 @@
 # for more information.
 
 library(dplyr)
+library(tidyr)
 library(quantregForest)
 library(purrr)
 
-make.model <- function(td, MTRY=4, NTREE=3000, NTHREADS=1, ...) {
+make.model <- function(td, ntree=2000, ...) {
   # This function trains a temperature model on the provided dataset td.
   # In addition to a number of predictors for which the name is irrelevant
   # so long as it is consistent (see paper for specifics), the input should
@@ -31,8 +32,9 @@ make.model <- function(td, MTRY=4, NTREE=3000, NTHREADS=1, ...) {
   # columns, predictor columns can have arbitrary names and can be a mix
   # of numerical and categorical inputs.
   # 
-  # The arguments MTRY, NTREE, NTHREADS, and ... are passed on directly
+  # The arguments ntree and ... are passed on directly
   # to quantregForest.
+  # Suggested: specify `nthreads` for multi-threading
   #
   # The output of this function is a list of monthly models, named by month
   # as a two-character string (e.g. "05").  This will actually subdivide by
@@ -50,7 +52,7 @@ make.model <- function(td, MTRY=4, NTREE=3000, NTHREADS=1, ...) {
     x <- select(dat, -temperature)
     y <- dat$temperature
     mod <- if (nrow(dat) > 10) {
-      quantregForest(x, y, mtry=MTRY, ntree=NTREE, nthreads=NTHREADS, ...)
+      quantregForest(x, y, ntree=ntree, ...)
     } else NULL
     regl[[month]] <- mod
   }
@@ -77,6 +79,8 @@ predict.temperature <- function(mod, data, compare=F, preserve=F, what=NULL) {
   # If preserve is TRUE, the output will simply keep the input data frame
   # and add a predicted temperature column.  Otherwise, only the named
   # columns required above will be preserved.
+  #
+  # NOTE: rows with NAs will be dropped.
   data <- drop_na(data) %>% urban
   wlabel <- !is.null(what)
   what <- if (!wlabel) 0.5 else what
@@ -142,4 +146,102 @@ urban <- function(td, threshold=0.1, isolate=F, nurb=F) {
     cat("\nNum. non-urban gages: ", length(unique(td$id[!urb])))
     td
   }
+}
+
+case.study <- function() {
+  # This is the source code of the case study from the paper.
+  # (Philippus, Sytsma and Hogue, not yet submitted.)
+  # 
+  # We retrieved river temperatures for 100 streams with an estimated
+  # width of at least 1 m in the Colorado Rocky Mountains (2014-2021).
+  # Then we evaluated trends in cutthroat trout suitability, based on
+  # lethal temperature (20-24 C, using 20) and optimal reproduction
+  # temperature (> 8 C in July).
+  #
+  # The model is trained on the calibration dataset provided.
+  library(tidyverse)  # for data manipulation, plotting, etc
+  source("util.R")  # helper functions: plot.theme(), print.and.png(), cal.data()
+  
+  cal <- cal.data()
+  model <- make.model(cal, nthreads=16)  # nthreads to speed up training
+  
+  trout.data <- read_csv("TempestTrout.csv") %>%
+    replace_na(list(barren=0, builtup=0, cropland=0, grassland=0,
+                    mangrove=0, moss=0, shrubland=0, snow=0, trees=0,
+                    water=0, wetland=0))  # missing land cover data - fixed in newer GEE script
+  trout <- predict.temperature(model, trout.data)
+  
+  is.survivable <- function(year.temps) {
+    # Boolean: assuming the data provided are for one full year,
+    # is the river survivable for that year?
+    max(year.temps, na.rm = TRUE) < 293  # Kelvin
+  }
+  is.optimal <- function(months, year.temps) {
+    # Boolean: is the July temperature in the optimal range?
+    july <- year.temps[months == "07"]
+    if (length(july) == 0)
+      FALSE
+    else {
+      july[1] >= 281
+    }
+  }
+  egg <- function(months, year.temps) {
+    jan <- year.temps[months == "01"]
+    if (length(jan) == 0)
+      FALSE
+    else {
+      jan[1] >= 275  # Kelvin
+    }
+  }
+  
+  suitability <- trout %>%
+    group_by(id, year) %>%
+    summarize(Survivable=is.survivable(temperature),
+              Optimal=is.optimal(time, temperature),
+              Eggs=egg(time, temperature),
+              Both=Optimal & Survivable & Eggs) %>%
+    group_by(year) %>%
+    summarize(
+      Survivable=mean(Survivable) * 100,
+      Eggs=mean(Eggs) * 100,
+      All=mean(Both) * 100,
+      Optimal=mean(Optimal) * 100  # percentages
+    )
+  print(suitability)
+  names(suitability) <-
+    c("year",
+      "Accute Maximum",
+      "Optimal Egg Viability",
+      "Suitable Year-Round",
+      "Optimal Fry Survival"
+      )
+  suitability <- suitability %>%
+    pivot_longer(
+      c("Accute Maximum",
+        "Optimal Fry Survival",
+        "Optimal Egg Viability",
+        "Suitable Year-Round"),
+      names_to="Type",
+      values_to="Suitability"
+    )
+  
+  restab <- suitability %>%
+    ungroup() %>%
+    group_by(Type) %>%
+    summarize(
+      R2 = cor(year, Suitability)^2,
+      pval = cor.test(year, Suitability)$p.value,
+      Coeff = lm(Suitability ~ year)$coefficients[[2]]
+    )
+  print(restab)
+  suitability %>%
+    ggplot() +
+    aes(x=year) +
+    geom_col(aes(y=Suitability, fill=Type), position = "dodge") +
+    plot.theme() +
+    labs(
+      x="Year",
+      y="Percent of Streams in Suitability Range",
+      fill="Suitability Type"
+    )
 }
